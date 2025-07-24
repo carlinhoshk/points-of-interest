97c6f06bdb005ae8f6b637917fdf402ea7edc004
carlinhoshk <carlosmdohk@gmail.com>
2025-07-24 16:01:46 -0300

    ✨ feat: Inicializa projeto com configuração H2 e validação.

---
+++ src/main/java/io/github/carlinhoshk/GPS_API/GpsApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.carlinhoshk.GPS_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsApiApplication.class, args);
+	}
+
+}
